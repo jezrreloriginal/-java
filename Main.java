65a0ae05af73939d37a43aa5f800ae4cd9312db5
jezrreloriginal <124934158+jezrreloriginal@users.noreply.github.com>
2023-05-24 15:54:36 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("olá mundo");
+    int myNum = 67;
+    float myFloatNum = 678.90f;
+    char myLetter = 'j';
+    boolean myBool = false;
+    String myText = "foi caralho";
+    System.out.println(myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText);
+  //parte dois do codigo
+
+    System.out.println(myText + myBool);
+    
+     double myDouble = myInt;
+    
+
+
+   
+
+    
+
+    
+  }
+}
