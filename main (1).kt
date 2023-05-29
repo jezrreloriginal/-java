27f8d93704dc6113137dcd7445ada554613529c9
jezrreloriginal <124934158+jezrreloriginal@users.noreply.github.com>
2023-05-29 16:18:19 -0300

    Add files via upload
    operações kotlin

---
+++ main (1).kt
@@ -0,0 +1,23 @@
+fun main () {
+
+// OPERAÇÕES MATEMATICAS 
+
+var x = 13
+var y = 4
+  
+var soma=x+y
+println("a soma é:" + soma)
+
+
+var divisao=x/y
+println("a divisão é:" + divisao)
+
+var sub=x-y
+println("a subtração é:" + sub)
+
+var mult=x*y
+  println("a multiplicação é:" + mult)
+
+var por=x%y
+println("a porcentagem é:" + por)
+
